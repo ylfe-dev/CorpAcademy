c2b6ce033d10e67f6f82452bebacde1da3376b38
Ryszard Raby <ryszard.raby@ceneo.pl>
2024-09-28 16:20:01 +0200

    categories

--- frontend/src/containers/Menu/menu.jsx
+++ frontend/src/containers/Menu/menu.jsx
@@ -1,6 +1,7 @@
 import Hint from '../../components/Hint/hint'
 import { Outlet, Link } from "react-router-dom";
 import Scene from '../../components/Scene/scene';
+import Categories from '../../components/Categories/categories';
 
 function Menu (){  
     return (
@@ -9,16 +10,40 @@ function Menu (){
             </section>
         
             <section className='content'>
-                <Hint character="cat" text="W co zagramy?"/>
+                <Categories categories={[
+                    { name: '🏖️' }, 
+                    { name: '🚃' }, 
+                    { name: '🛤️' },
+                    { name: '🏞️' },
+                    { name: '🏙️' },
+                    { name: '🏜️' },
+                    { name: '🏝️' },
+                    { name: '🏔️' },
+                    { name: '🏟️' },
+                    { name: '🏠' },
+                    { name: '🏛️' },
+                    { name: '🏰' },
+                    { name: '🏚️' },
+                    { name: '🏗️' },
+                    { name: '🏭' },
+                    { name: '🏢' },
+                    { name: '🏬' },
+                    { name: '🏣' },
+                    { name: '🏥' },
+                    { name: '🏦' },
+                    { name: '🏪' },
+                    { name: '🏫' },
+                    { name: '🏩' },
+                    { name: '🏨' },                    
+                    ]}/>
+                <Hint left character="cat" text="W co zagramy?"/>
             </section>
             
             <section className='action'>
                 <Link className="button" to={'/game'}>Start Gry</Link>
             </section>
         </Scene>      
-
         )
-       
 }
 
 export default Menu
